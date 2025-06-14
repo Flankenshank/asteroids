90ae7837e2a2771c36d5679b1b55440f953bcc95
Flankenshank <dylan.date@gmail.com>
2025-06-14 17:52:53 +0200

    Added read file

--- llm/functions/get_files_info.py
+++ llm/functions/get_files_info.py
@@ -23,3 +23,26 @@ def get_files_info(working_directory, directory=None):
         return "\n".join(files_info)
     except Exception as e:
         return f"Error listing files: {e}"
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(os.path.abspath(working_directory), file_path))
+    if not (abs_file_path == abs_working_dir or abs_file_path.startswith(abs_working_dir + os.sep)):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    try:
+        MAX_CHARS = 10000
+        with open(abs_file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            extra = f.read(1)
+            if extra != '':
+                file_content_string += '[...File "{file_path}" truncated at 10000 characters]'
+            return file_content_string
+
+    except Exception as e:
+        return f"Error: {e}"
+    
+    
--- llm/tests.py
+++ llm/tests.py
@@ -1,24 +1,25 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content
 
 
 def test():
-    result = get_files_info("calculator", ".")
+    result = get_file_content("calculator", "main.py")
     print("Result for current directory:")
     print(result)
     print("")
 
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
-    print(result)
+def test2():
+    result2 = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for current directory:")
+    print(result2)
+    print("")
 
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin' directory:")
-    print(result)
-
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
-    print(result)
-
+def test3():
+    result3 = get_file_content("calculator", "/bin/cat")
+    print("Result for current directory:")
+    print(result3)
+    print("")
 
 if __name__ == "__main__":
     test()
+    test2()
+    test3()
